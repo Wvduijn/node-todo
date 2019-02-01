635dde0b744e9f1d5a6287def45ba5c5d2c4f2fb
Willem van Duijn <willem.vanduijn@kpn.com>
2019-02-01 13:57:46 +0100

    Made get and post private

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // POST
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then(
@@ -35,8 +36,10 @@ app.post('/todos', (req, res) => {
 });
 
 // GET
-app.get('/todos', (req, res) => {
-  Todo.find().then(
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then(
     todos => {
       res.send({ todos });
     },
