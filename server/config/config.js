5989908ee229d9577991456705f6c857c25eac00
Willem van Duijn <willem.vanduijn@kpn.com>
2019-02-01 15:27:06 +0100

    Update config to use JSON

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,15 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ****', env);
 
-if (env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if(env ==='test'){
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-} else {
-    process.env.MONGODB_URI = 'mongodb://pixelbender:PixelBender1@ds141720.mlab.com:41720/todoappnode';
+if (env === 'development' || env === 'test'){
+ var config = require('./config.json');
+ var envConfig = config[env];
+
+ Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+ });
 }
+// } else {
+//     process.env.MONGODB_URI = 'mongodb://pixelbender:PixelBender1@ds141720.mlab.com:41720/todoappnode';
+// }
 
